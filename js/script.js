6d12e2bf9271079fa8098bbccc48db3a838de8c2
Ana Novković <anaiscoding@gmail.com>
2024-11-18 10:15:38 +0100

    Fix js bug

--- js/script.js
+++ js/script.js
@@ -3,6 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const closeButton = document.querySelector(".menu__button-close");
     const menu = document.querySelector(".menu");
 
+    const handleResize = () => {
+        if (window.innerWidth >= 1024) { 
+            menu.classList.remove("display");
+            hamburgerButton.style.display = "none"; 
+            closeButton.style.display = "none";
+        } else {
+            hamburgerButton.style.display = "block"; 
+            closeButton.style.display = "none"; 
+        }
+    };
+
     if (hamburgerButton && closeButton && menu) {
         hamburgerButton.addEventListener("click", () => {
             menu.classList.add("display"); 
@@ -15,8 +26,11 @@ document.addEventListener("DOMContentLoaded", () => {
             closeButton.style.display = "none"; 
             hamburgerButton.style.display = "block"; 
         });
+
+        window.addEventListener("resize", handleResize);
+
+        handleResize();
     } else {
         console.error("Required elements are missing in the DOM.");
     }
 });
-
